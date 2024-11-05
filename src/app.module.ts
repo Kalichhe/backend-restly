b7fded9f904d54f634b7ea1dd0c2503d2bce61f2
kalichhe <jhondh2003@gmail.com>
2024-11-05 15:29:12 -0500

    Create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ProfileModule } from './profile/profile.module';
+import { PropertiesModule } from './properties/properties.module';
+import { ExploreModule } from './explore/explore.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ProfileModule, PropertiesModule, ExploreModule, BookingsModule, ReviewsModule],
 })
 export class AppModule {}
